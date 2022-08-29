4dfab47c159f29e333093d25b6ea204a38cc6c01
10cat <ylincath10@gmail.com>
2022-08-29 12:13:24 -0600

    Add files via upload
    config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+dataset_root = "/home/datassd/yilin/GRAB_processed/GRAB_V01/"
+
+model_root = "/home/datassd/yilin/Codes/_toolbox/smpl_models/"
+
+gender_map = {
+    "s1": 'male',
+    "s2": 'male',
+    "s3": 'female',
+    "s4": 'female',
+    "s5": 'female',
+    "s6": 'female',
+    "s7": 'female',
+    "s8": 'male',
+    "s9": 'male',
+    "s10": 'male',
+}
