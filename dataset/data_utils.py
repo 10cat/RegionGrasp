569a5adc7116934b267e05108355ed4c0cbf9b0e
10cat <ylincath10@gmail.com>
2022-11-30 19:14:49 -0700

    thumb condition region computation

--- dataset/data_utils.py
+++ dataset/data_utils.py
@@ -9,18 +9,59 @@ import config
 from utils.visualization import colors_like
 import matplotlib.pyplot as plt
 
+class Struct(object):
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+            
+def load_mano_model(model_path, is_rhand=True, ext='pkl'):
+    import os.path as osp
+    import pickle
+    
+    # Load the model
+    if osp.isdir(model_path):
+        model_fn = 'MANO_{}.{ext}'.format('RIGHT' if is_rhand else 'LEFT', ext=ext)
+        mano_path = os.path.join(model_path, model_fn)
+    else:
+        mano_path = model_path
+        
+    assert osp.exists(mano_path), 'Path {} does not exist!'.format(
+        mano_path)
 
-def visual_inter(hand_mesh, 
-                 rh_fs, 
-                 obj_mesh, 
-                 obj_fs, 
+    if ext == 'pkl':
+        with open(mano_path, 'rb') as mano_file:
+            model_data = pickle.load(mano_file, encoding='latin1')
+    elif ext == 'npz':
+        model_data = np.load(mano_path, allow_pickle=True)
+    else:
+        raise ValueError('Unknown extension: {}'.format(ext))
+    data_struct = Struct(**model_data)
+    return data_struct
+
+def visual_mesh_region(mesh, fs, color):
+    mesh.visual.face_colors[fs] = colors_like(config.colors[color])
+
+def visual_mesh(mesh, bg_color='grey', mark_region=None, mark_color='yellow'):
+    mesh.visual.face_colors = colors_like(config.colors[bg_color])
+    if mark_region:
+        if isinstance(mark_region[0], list):
+            assert isinstance(mark_color, list), "Parameter 'mark_color' must match the type of 'mark_region'!"
+            for idx, region in enumerate(mark_region):
+                visual_mesh_region(mesh, region, mark_color[idx])
+                # mesh.visual.face_colors[region] = colors_like(config.colors[mark_color[idx]])
+                
+        else:
+            assert isinstance(mark_color, str), "Parameter 'mark_color' must match the type of 'mark_region'!"
+            visual_mesh_region(mesh, mark_region, mark_color)
+            # mesh.visual.face_colors[mark_region] = colors_like(config.colors[mark_color])
+
+def visual_inter(hand_mesh, rh_fs, h_mark_color,
+                 obj_mesh, obj_fs, o_mark_color,
                  output_folder, 
                  frame_name):
-    hand_mesh.visual.face_colors = colors_like(config.colors['skin'])
-    obj_mesh.visual.face_colors = colors_like(config.colors['grey'])
-    
-    hand_mesh.visual.face_colors[rh_fs] = colors_like(config.colors['red'])
-    obj_mesh.visual.face_colors[obj_fs] = colors_like(config.colors['blue'])
+    # TODO: component related visualization
+    visual_mesh(hand_mesh, bg_color='skin', mark_region=rh_fs, mark_color=h_mark_color)
+    visual_mesh(obj_mesh, bg_color='grey', mark_region=obj_fs, mark_color=o_mark_color)
     
     output_path_hand = os.path.join(output_folder, frame_name+'_hand.ply')
     output_path_obj = os.path.join(output_folder, frame_name+'_obj.ply')
@@ -132,4 +173,49 @@ def inner_verts_detect(mesh_dest, mesh_orig, vert_ids):
     signed_dists = trimesh.proximity.signed_distance(mesh_dest, verts)
     inner_indices = np.where(signed_dists > 0)[0].tolist()
     return inner_indices
+
+def find_surrounded_faces(vids, mesh):
+    face_ids = []
+    for vid in vids:
+        fids_arr = mesh.vertex_faces[vid]
+        fids_arr = fids_arr[fids_arr > 0] # unpadded
+        face_ids += fids_arr.tolist()
+        face_ids = list(set(face_ids))
+    return face_ids
+
+
+if __name__ == "__main__":
+    mano_path = config.mano_dir
+    mano_model = load_mano_model(mano_path)
+    
+    rh_verts = mano_model.v_template
+    rh_faces = mano_model.f
+    hand_mesh = trimesh.Trimesh(vertices=rh_verts, faces=rh_faces)
+    
+    thumb_faces = find_surrounded_faces(config.thumb_vertices, hand_mesh)
+    index_faces = find_surrounded_faces(config.indexfinger_vertices, hand_mesh)
+    middle_faces = find_surrounded_faces(config.middlefinger_vertices, hand_mesh)
+    fourth_faces = find_surrounded_faces(config.fourthfinger_vertices, hand_mesh)
+    small_faces = find_surrounded_faces(config.smallfinger_vertices, hand_mesh)
+    
+    visual_mesh(hand_mesh, 
+                bg_color='skin', 
+                mark_region=[thumb_faces, index_faces, middle_faces, fourth_faces, small_faces], 
+                mark_color=['green', 'blue', 'purple', 'yellow', 'orange'])
+    
+    # hand_mesh.show()
+    
+    hand_comp = {'thumb':[thumb_faces, config.thumb_vertices], 
+                 'index': [index_faces, config.indexfinger_vertices], 
+                 'middle': [middle_faces, config.middlefinger_vertices], 
+                 'fourth': [fourth_faces, config.fourthfinger_vertices], 
+                 'small': [small_faces, config.smallfinger_vertices]}
+    
+    output_path = 'dataset/tools/hand_comp.npz'
+    np.savez(output_path, hand_comp, allow_pickle=True)
+    
+    
+    
+    
+    
 
--- dataset/thumb_condition.py
+++ dataset/thumb_condition.py
@@ -9,17 +9,18 @@ import trimesh
 import mano
 from mano.model import load
 from dataset.Dataset_origin import GrabNetDataset_orig
-from dataset.data_utils import m2m_intersect, visual_hist, visual_inter, visual_sort, faces2verts_no_rep, inner_verts_detect
+from dataset.data_utils import m2m_intersect, visual_hist, visual_inter, visual_mesh, visual_sort, faces2verts_no_rep, inner_verts_detect, visual_mesh_region
 from utils.utils import func_timer, makepath
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-
 class ThumbConditionator():
     def __init__(self, visual_folder, plot=False):
         super().__init__()
-        self.thumb_verts = config.thumb_vertices
+        # self.thumb_verts = config.thumb_vertices
+        self.visual_folder = visual_folder
+        self.plot = plot
         self.Contactor = ContactDetector(visual_folder=visual_folder, plot=plot)
         
     def contact_annot(self, hand_mesh, obj_mesh, frame_name):
@@ -27,29 +28,70 @@ class ThumbConditionator():
         
         return obj_contact_face_ids
     
-    def center_point(self):
-        return
+    def thumb_condition_region(self, obj_mesh, face_ids_dict, frame_name, region_depth=3):
+        # 'center'-> contact region detect返回的obj_face_ids列表最后一个
+        # NOTE: contact region determination采用BFS向内查询添加面，所以返回的obj_face_ids列表最后的一个面一定是区域中心
+        thumb_obj_faces = face_ids_dict['thumb']
+        center_face_id = thumb_obj_faces[-1]
+        center_face = obj_mesh.faces[center_face_id]
+        vertex_ids = center_face.reshape(-1).tolist()
+        face_ids = [center_face_id]
+        depth = 0
+        vids_search = vertex_ids
+        while(depth < region_depth):
+            new_face_ids = []
+            for vid in vids_search:
+                fids = obj_mesh.vertex_faces[vid]
+                fids = fids[fids > 0].tolist()
+                # import pdb; pdb.set_trace()
+                new_face_ids = list(set(new_face_ids + fids) - set(face_ids))
+            face_ids += new_face_ids
+            new_vert_ids = faces2verts_no_rep(obj_mesh.faces[new_face_ids])
+            new_vert_ids = list(set(new_vert_ids) - set(vertex_ids))
+            vertex_ids += new_vert_ids
+            vids_search = new_vert_ids
+            depth += 1
+        
+        thumb_condition = {
+            'center': center_face_id,
+            'faces': face_ids,
+            'verts': vertex_ids
+        }
+        # import pdb; pdb.set_trace()
+        # test visualization result
+        visual_mesh_region(obj_mesh, thumb_condition['faces'], 'pink')
+        visual_mesh_region(obj_mesh, thumb_condition['center'], 'yellow')
+        output_path = os.path.join(self.visual_folder, frame_name + '_filled_obj_cond.ply')
+        obj_mesh.export(output_path)
+        # import pdb; pdb.set_trace()
+        
+        return thumb_condition
     
-    def condition_region(self):
-        return
-    
-    def save_annotate(self):
+    def save_annotate(self, contact_faces_dict, thumb_condition_dict):
+        save_dict = {
+            'contact': contact_faces_dict,
+            'thumb_cond': thumb_condition_dict
+        }
+        
+        
         return
     
     def run(self, hand_verts, hand_faces, obj_verts, obj_faces, frame_name):
         HandMesh = trimesh.Trimesh(vertices=hand_verts, faces=hand_faces)
         ObjMesh = trimesh.Trimesh(vertices=obj_verts, faces=obj_faces)
         
-        # TODO: I.Contact Region Determination
-        self.contact_annot(HandMesh, ObjMesh, frame_name)
+        # I.Contact Region Determination
+        o_contact_face_ids_dict = self.contact_annot(HandMesh, ObjMesh, frame_name)
         
-        # TODO: II. Select a 'center' of the contact region
-        self.center_point()
+        thumb_condition_dict = self.thumb_condition_region(ObjMesh, o_contact_face_ids_dict, frame_name)
         
-        # TODO: III. Select the 'smallest' 'neighborhood' as the condition region
-        self.condition_region()
+        # II. Select one 'center' of the contact region
+        # self.center_point()
         
-        # TODO: IV. 转换成可以存储的标注形式 + 存储标注
+        # III. Select the 'smallest' 'neighborhood' as the condition region
+        # self.condition_region()
+        
+        # IV. 转换成可以存储的标注形式 + 存储标注
         self.save_annotate()
         
         
@@ -60,6 +102,7 @@ class ContactDetector():
         super().__init__()
         self.visual_folder = visual_folder
         self.plot = plot
+        self.hand_comps_name = ['thumb', 'index', 'middle', 'fourth', 'small', 'palm']
         
     def intersection_detect(self, hand_mesh, obj_mesh):
         ContactDict = m2m_intersect(hand_mesh, 'hand', obj_mesh, 'obj')
@@ -95,32 +138,71 @@ class ContactDetector():
                 st_verts_dict[st_vert].append(face.reshape(1, -1))
                 
         st_vert_list = sorted(list(st_verts_dict.keys()))
-        #TODO: form sorted face numpy array
+        # form sorted face numpy array
             
         # visual_sort(depth_array, plot=self.plot)
         
         return
 
 
-    @func_timer
-    def fill_interior(self, hand_mesh, obj_mesh, ContactDict):
-        """without cluster"""
-        face_ids = ContactDict['face_index']['obj']
-        face_ids = list(set(face_ids))
-        obj_faces = obj_mesh.faces[face_ids]
+    def group_by_hand_comp(self, ContactDict):
+        h_face_ids = ContactDict['face_index']['hand']
+        o_face_ids = ContactDict['face_index']['obj']
+        h_comp_face_ids = {'thumb':[], 'index':[], 'middle':[], 'fourth':[], 'small':[], 'palm':[]}
+        o_comp_face_ids = {'thumb':[], 'index':[], 'middle':[], 'fourth':[], 'small':[], 'palm':[]}
         
-        # TODO: find the inner contour of the face by signed distance
-        vert_ids = faces2verts_no_rep(obj_faces)
-        inner_indices = inner_verts_detect(hand_mesh, obj_mesh, vert_ids)
-        inner_vert_ids = [vert_ids[id] for id in inner_indices]
-        # TODO: BFS search for interior faces;
+        comps = ['thumb', 'index', 'middle', 'fourth', 'small']
+        for idx, rh_fid in enumerate(h_face_ids):
+            for comp in comps:
+                count = 0
+                if rh_fid in config.hand_comp[comp][0]:
+                    count += 1
+                    obj_fid = o_face_ids[idx]
+                    if obj_fid not in o_comp_face_ids[comp]:
+                        o_comp_face_ids[comp].append(obj_fid)
+                        h_comp_face_ids[comp].append(rh_fid)
+                    break
+            if count == 0:
+                if o_face_ids[idx] not in o_comp_face_ids['palm']:
+                    o_comp_face_ids['palm'].append(obj_fid)
+                    h_comp_face_ids['palm'].append(rh_fid)
+        ContactDict['face_index']['hand'] = h_comp_face_ids
+        ContactDict['face_index']['obj'] = o_comp_face_ids             
+            
+    @func_timer
+    def fill_interior(self, hand_mesh, obj_mesh, face_ids_dict):
+        """without cluster"""
+        
+        # 1. find the inner contour of the face by signed distance
+        inner_vert_ids = []
+        inner_vert_comps = []
+        face_ids = [] # NOTE: needed！the total face list used for BFS
+        for idx, comp in enumerate(self.hand_comps_name):
+            # first get the non-repetitive faces & inner_vertices for each part
+            # import pdb; pdb.set_trace()
+            comp_face_ids = face_ids_dict[comp]
+            if comp_face_ids:
+                obj_faces = obj_mesh.faces[comp_face_ids]
+                vert_ids = faces2verts_no_rep(obj_faces)
+                inner_indices = inner_verts_detect(hand_mesh, obj_mesh, vert_ids)
+                comp_inner_vert_ids = [vert_ids[id] for id in inner_indices] # NOTE: vert_ids is list, cannot be directly referenced with list
+                # update vert list and face list
+                inner_vert_ids += comp_inner_vert_ids
+                face_ids += comp_face_ids
+                # obtain comp marks for inner_vert
+                inner_vert_comps += [idx] * len(comp_inner_vert_ids)
+                # face_comps += [idx] * len(comp_face_ids)
+            
+        # 2. BFS search for interior faces;
         ## 更新对象： face_ids (list)
         verts_in_search = inner_vert_ids
+        verts_comps = inner_vert_comps
         iter_num = 1
         print("Start BFS search!")
         while(verts_in_search):
-            new_face_ids = []
-            for vid in tqdm(verts_in_search, desc=f'depth: {iter_num}'):
+            new_vert_ids = [] # need a vert list
+            new_vert_comps = []
+            for idx, vid in enumerate(tqdm(verts_in_search, desc=f'depth: {iter_num}')):
                 v_fids = obj_mesh.vertex_faces[vid] # returns: (m,) m = max number of faces for a single vertex in the mesh, padded with -1 
                 v_fids = v_fids[v_fids > 0] # unpadded
                 for fid in v_fids:
@@ -128,45 +210,75 @@ class ContactDetector():
                         face = obj_mesh.faces[fid]
                         vids = face.reshape(-1).tolist()
                         inners = inner_verts_detect(hand_mesh, obj_mesh, vids)
-                        if len(inners) < len(vids):
+                        if len(inners) < 3: # 必须要3个点都被判定为在contour内部才能添加
                             continue
+                        
+                        # update comp list based on the comp of the current vert
+                        comp = verts_comps[idx]
+                        # update vert list
+                        for v in vids:
+                            if v not in vert_ids:
+                                vert_ids.append(v) # NOTE: must update vert_ids, or there will be repetitive search!
+                                new_vert_ids.append(v)
+                                new_vert_comps.append(comp)
+                        # update face_ids_dict directly
                         face_ids.append(fid)
-                        new_face_ids.append(fid)
+                        face_ids_dict[self.hand_comps_name[comp]].append(fid)
+                        
+                        
             # import pdb; pdb.set_trace()
-            if new_face_ids:
-                new_obj_faces = obj_mesh.faces[new_face_ids]
-                new_vert_ids = faces2verts_no_rep(new_obj_faces)
+            if new_vert_ids:
                 verts_in_search = new_vert_ids
+                verts_comps = new_vert_comps
             else:
                 verts_in_search = [] 
             
             iter_num += 1
-        
-        
-        
-        return face_ids
+            
+        return face_ids_dict
     
     def run(self, hand_mesh, obj_mesh, frame_name):
         # NOTE: I.-1.1 Mesh intersection detection on thumb region
         contact_dict = self.intersection_detect(hand_mesh, obj_mesh)
         
         
-        visual_inter(hand_mesh, contact_dict['face_index']['hand'], 
-                     obj_mesh, contact_dict['face_index']['obj'], 
+        visual_inter(hand_mesh, contact_dict['face_index']['hand'],'red',
+                     obj_mesh, contact_dict['face_index']['obj'], 'blue', 
                      output_folder=self.visual_folder, 
                      frame_name=frame_name)
         
-        obj_contact_face_ids = self.fill_interior(hand_mesh, obj_mesh, contact_dict)
+        # divide contact regions based on hand component
+        self.group_by_hand_comp(contact_dict)
         
-        visual_inter(hand_mesh, contact_dict['face_index']['hand'], 
-                     obj_mesh, obj_contact_face_ids, 
+        # TODO: combine the obj_face_ids for BFS convenience, but with comp marks
+        o_face_ids_dict = contact_dict['face_index']['obj']
+            
+        o_contact_face_ids_dict = self.fill_interior(hand_mesh, obj_mesh, o_face_ids_dict)
+        
+        
+        # obj_contact_face_ids = {}
+        # for comp in self.hand_comps_name:
+        #     obj_face_ids = contact_dict['face_index']['obj'][comp]
+        #     if obj_face_ids:
+        #         # input 'face_ids' from obj that correspond to different components one by one
+        #         # too slow!!! need to still do the full BFS for fast speed => full BFS but with comp mark?
+        #         obj_comp_face_ids = self.fill_interior(hand_mesh, obj_mesh, obj_face_ids)
+        #         obj_contact_face_ids[comp] = obj_comp_face_ids
+        #     else:
+        #         obj_contact_face_ids[comp] = []
+        
+        # NOTE: visualize
+        mark_colors = config.hand_comp_colors   
+        h_face_ids = list(contact_dict['face_index']['hand'].values())
+        obj_contact_face_ids = list(o_contact_face_ids_dict.values())
+        visual_inter(hand_mesh, h_face_ids, mark_colors,
+                     obj_mesh, obj_contact_face_ids, mark_colors, 
                      output_folder=self.visual_folder, 
                      frame_name=frame_name+'_filled')
+        import pdb; pdb.set_trace()
         
         # NOTE: I.-1.2 Clustering intersection points into intersection rings
         # self.cluster_rings(contact_dict, obj_mesh)
-        
-        
         
         
         # TODO: I.-1.3 Hand Mesh Segmentation based on rings
@@ -177,11 +289,12 @@ class ContactDetector():
         # TODO: I.-2 If not intersecting with object, threshold signed distance -> contact faces
         
         
-        return obj_contact_face_ids
+        return o_contact_face_ids_dict
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('--ds_name', required=True)
     parser.add_argument('--plot', action='store_true', default=False)
     args = parser.parse_args()
     
@@ -193,24 +306,24 @@ if __name__ == "__main__":
                              num_pca_comps=45,
                              flat_hand_mean=True)
     
-    trainset = GrabNetDataset_orig(dataset_root, ds_name='train', dtype=np.float32)
+    dataset = GrabNetDataset_orig(dataset_root, ds_name=args.ds_name, dtype=np.float32)
     
     visual_folder = os.path.join(config.dataset_visual_dir, 'thumb_condition', 'intersect')
     makepath(visual_folder)
     
     Conditionator = ThumbConditionator(visual_folder=visual_folder, plot=args.plot)
     
-    for idx in range(trainset.__len__()):
+    for idx in range(dataset.__len__()):
         
-        sample = trainset.__getitem__(idx)
+        sample = dataset.__getitem__(idx)
         hand_faces = rh_model.faces
         hand_verts = sample['verts_rhand']
         
         
-        obj_name = trainset.frame_objs[idx]
+        obj_name = dataset.frame_objs[idx]
         # import pdb; pdb.set_trace()
         #NOTE:下采样到2048点的物体顶点不能直接使用，需要从原模型获得全部顶点坐标
-        ObjMesh = trainset.object_meshes[obj_name]
+        ObjMesh = dataset.object_meshes[obj_name]
         ## 先从原物体模型获得原物体顶点
         obj_verts_orig = ObjMesh.vertices 
         obj_trans = sample['trans_obj']
